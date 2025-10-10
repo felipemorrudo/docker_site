f002a2ed7651e94969e0c25940fdb4f9cdc02283
felipemorrudo <FELIPE.MANILA@GMAIL.COM>
2025-10-10 17:04:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app=Flask(__name__ )
 
 @app.route('/')
 def hello_world():
-    return '<h1>Olá diretamente do meu site criado com Docker!</h1><p>Isso está rodando dentro de um container!</p>'
+    return '<h1>CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC!</h1><p>Isso está rodando dentro de um container!</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
     
