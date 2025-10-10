cf4223b5cb7108addfc6d0654c6e291e0a8bdf48
felipemorrudo <FELIPE.MANILA@GMAIL.COM>
2025-10-10 17:06:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app=Flask(__name__ )
 
 @app.route('/')
 def hello_world():
-    return '<h1>CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC!</h1><p>Isso está rodando dentro de um container!</p>'
+    return '<h1>Olá, olá olá olá olá!</h1><p>Isso está rodando dentro de um container!</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
